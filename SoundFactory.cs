246bccf666ab9a593e6e86ceee68c8fb6dd646ef
BBQGiraffe <alexanderortizbusiness@gmail.com>
2021-08-28 12:54:36 -0500

    fix Git issues

--- SoundFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace Snowball
-{
-    public abstract class SoundFactory
-    {
-        public abstract SoundSource CreateSource(string fileName);
-    }
-}
--- demo/lua/levelloader.lua
+++
@@ -1,9 +0,0 @@
-switch = require("switchstatement")
-local levelloader = {}
-
-function levelloader:load_level(level)
-
-end
-
-
-return levelloader
